b7d23ea8fc197afba8827cc1184741735842db25
Lupino <lmjubuntu@gmail.com>
2020-11-17 16:36:15 +0800

    cache no need model_name

--- hotgym/cache.py
+++ hotgym/cache.py
@@ -1,20 +1,16 @@
 import time
 
 class CacheItem(object):
-    def __init__(self, name, model_name, model = None):
+    def __init__(self, name, model = None):
         self.name = name
         self.model = model
-        self.model_name = model_name
         self.timestamp = time.time()
 
     def __eq__(self, item):
-        return self.name == item.name and self.model_name == item.model_name
+        return self.name == item.name
 
     def get_name(self):
         return self.name
-
-    def get_model_name(self):
-        return self.model_name
 
     def get_model(self):
         self.timestamp = time.time()
@@ -29,7 +25,6 @@ class CacheItem(object):
 
 class Cache(object):
     def __init__(self, size=10):
-        # self.manager = manager
         self.size = size
         self.items = []
         self.saves = []
@@ -46,19 +41,17 @@ class Cache(object):
             self.saves.append(items[0])
             self.items.remove(items[0])
 
-    def get(self, name, model_name='model'):
+    def get(self, name):
         for item in self.items:
-            if item.get_name() == name and item.get_model_name() == model_name:
+            if item.get_name() == name:
                 return item
 
         return None
 
     def get_items(self):
-        print(self.items)
         return self.items
 
     def get_saves(self):
-        print(self.saves)
         saves = self.saves[:]
         self.saves = []
         return saves
