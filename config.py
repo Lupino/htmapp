4d231c516998c2354a437e8997ec17677b232ce6
Lupino <lmjubuntu@gmail.com>
2020-11-18 15:49:38 +0800

    remove noused config

--- config.py
+++ config.py
@@ -2,14 +2,12 @@ model_root='models'
 periodic_port='tcp://127.0.0.1:5000'
 
 parameters = {
-  # there are 2 (3) encoders: "value" (RDSE) & "time" (DateTime weekend, timeOfDay)
  'enc': {
       "value" :
          {'resolution': 0.88, 'size': 700, 'sparsity': 0.02},
       "time":
          {'timeOfDay': (30, 1), 'weekend': 21}
  },
- 'predictor': {'sdrc_alpha': 0.1},
  'sp': {'boostStrength': 3.0,
         'columnCount': 1638,
         'localAreaDensity': 0.04395604395604396,
@@ -26,11 +24,4 @@ parameters = {
         'newSynapseCount': 32,
         'permanenceDec': 0.1,
         'permanenceInc': 0.1},
- 'anomaly': {
-   'likelihood':
-       {#'learningPeriod': int(math.floor(self.probationaryPeriod / 2.0)),
-        #'probationaryPeriod': self.probationaryPeriod-default_parameters["anomaly"]["likelihood"]["learningPeriod"],
-        'probationaryPct': 0.1,
-        'reestimationPeriod': 100} #These settings are copied from NAB
- }
 }
