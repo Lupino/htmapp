070f74f691c2619dbd0245799b60f6fb820bf1ab
Lupino <lmjubuntu@gmail.com>
2020-11-25 11:08:52 +0800

    shellcheck run.sh

--- run.sh
+++ run.sh
@@ -1,37 +1,52 @@
 #!/usr/bin/env bash
 
-ROOT=$(cd $(dirname $0); pwd)
+ROOT=$(cd "$(dirname "$0")" || exit; pwd)
 INSTALL_LOCK=${ROOT}/.installed
 VENV_PATH=${ROOT}/venv
 
+cd "${ROOT}" || exit
 
 PYTHON=python3.8
 ${PYTHON} -c 'print()' >/dev/null 2>/dev/null || PYTHON=python3
 ${PYTHON} -c 'print()' >/dev/null 2>/dev/null || PYTHON=python
 
 install_required() {
-    ${PYTHON} -m pip install -U -r requirements.txt
-    shasum -a 256 requirements.txt > ${INSTALL_LOCK}
+    ${PYTHON} -m pip install -U -r "${ROOT}/requirements.txt"
+    shasum -a 256 "${ROOT}/requirements.txt" > "${INSTALL_LOCK}"
 }
 
 if [ -z "${SYSTEM_PYTHON}" ]; then
-    [ -d ${VENV_PATH} ] || ${PYTHON} -m venv ${VENV_PATH}
-    source ${VENV_PATH}/bin/activate
+    [ -d "${VENV_PATH}" ] || ${PYTHON} -m venv "${VENV_PATH}"
+    # shellcheck source=/dev/null
+    source "${VENV_PATH}/bin/activate"
 fi
 
-if [ -e ${INSTALL_LOCK} ]; then
-    shasum -a 256 -c ${INSTALL_LOCK} >/dev/null 2>/dev/null || install_required
+if [ -e "${INSTALL_LOCK}" ]; then
+    shasum -a 256 -c "${INSTALL_LOCK}" >/dev/null 2>/dev/null || install_required
 else
     install_required
 fi
 
-if [ "$1" == "yapf" ]; then
-    $@
-elif [ "$1" == "flake8" ]; then
-    $@
+VALIDCOMMAND=(
+    yapf
+    flake8
+    pip
+    pip3
+    python
+)
+
+for cmd in "${VALIDCOMMAND[@]}"; do
+    if [ "$1" == "${cmd}" ]; then
+        "$@"
+        exit 0
+    fi
+done
+
+if [ -z "$1" ]; then
+    ${PYTHON}
 elif [ "$1" == "cli" ]; then
     shift
-    ${PYTHON} script.py htmapp/cli.py $@
+    ${PYTHON} script.py htmapp/cli.py "$@"
 else
-    ${PYTHON} script.py $@
+    ${PYTHON} script.py "$@"
 fi
